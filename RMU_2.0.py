071239bea214f0cf28450b479df13d7d6844153a
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-05-05 10:46:29 +0300

    Обновлен CLI

--- RMU_2.0.py
+++ RMU_2.0.py
@@ -1,52 +1,138 @@
 import os, time, re
 import xml.etree.cElementTree as ET
+from distutils.util import strtobool
 import pandas as pd
-import string
-from textwrap import TextWrapper as TW
-start_time = time.time()
+#import string
+#from textwrap import TextWrapper as TW
+
+
 game = 'RimWorld'
-
-txt_list = {'gog': 'Версия игры в целевой папке: ',
-            'steam': 'Версия игры в папке Steam: ',
-            5: '       Автор: ',
-            6: '       Версия: ',
-            7: '       Поддерживает версии: ',
-            'q': 'Выход из программы',
-            'errinp': 'Некоректный ввод',
-            'wip': 'Функция в разработке'}
-
-menu = ['1. Догрузить недостающие моды в целевую папку WIP',
-        '2. Догрузить все моды в целевую папку WIP',
-        '3. Список модов установленных через Steam',
-        '4. Список модов установленных вручную',
-        '5. Список всех модов',
-        '6. Сравнение списка модов из папки Steam и целевой папки',
-        'Q. Выход', ]
-
-df = {'steam_mods': 'C:/Games/SteamLibrary/steamapps/workshop/content/294100',
-      'steam_game_dir': 'C:/Games/SteamLibrary/steamapps/common/RimWorld',
-      'target_directory': 'C:/Games/RimWorld',
-      'mods_directory': '/Mods',
-      'about': '/About/About.xml',
-      'manifest': '/About/Manifest.xml',
-      'steam_id': '/About/PublishedFileId.txt',
-      'version': '/Version.txt'}
-
+lang_pack = {'text':{'RU':{'inp':'\nВвод: ',
+                            'q': '\nВыход из программы',
+                            'errinp': 'Некоректный ввод',
+                            'wip': '\nФункция в разработке',
+                            'settings':'Доступные параметры:',
+                            'param_name':'Введите номер параметра для изменения: ',
+                            'param_value':'Изменить параметр на ',
+                            'err_param':'Недопустимый параметр',
+                            'available_param':'Возможные параметры',
+                            'gog':'Версия игры в целевой папке: ',
+                            'steam':'Версия игры в папке Steam: ',
+                            'about':'Это консольный модменеджер для RimWorld.\nПредназначен для просмотра списков модов и для \nперекидывания модов из Стима в папку RimWorld\nАвтор: CCF'}},
+             'menu':{'RU':{'main':{'1':'Догрузить недостающие моды в целевую папку',
+                                   '2':'Догрузить все моды в целевую папку',
+                                   '3':'Список модов установленных через Steam',
+                                   '4':'Список модов установленных вручную',
+                                   '5':'Список всех модов',
+                                   '6':'Сравнение списка модов из папки Steam и целевой папки',
+                                   'U':'Обновить списки',
+                                   'S':'Настройки',
+                                   'Q':'Q. Выход'},
+             'back_to_main':{'m':'Вернутся в главное меню'}}}}
+app_settings = {'1':{'language':'RU'},
+                '2':{'steam_mods': 'C:/Games/SteamLibrary/steamapps/workshop/content/294100'},
+                '3':{'steam_game_dir': 'C:/Games/SteamLibrary/steamapps/common/RimWorld'},
+                '4':{'target_directory': 'C:/Games/RimWorld'},
+                '5':{'mods_directory': '/Mods'},
+                '6':{'about': '/About/About.xml'},
+                '7':{'manifest': '/About/Manifest.xml'},
+                '8':{'steam_id':'/About/PublishedFileId.txt'},
+                '9':{'version': '/Version.txt'},
+                '10':{'sort_reversed':'False'},
+                '11':{'sort_tag':'name'}}
+available_settings = {'language':['RU'],
+                      'steam_mods': 'Anything',
+                      'steam_game_dir': 'Anything',
+                      'target_directory': 'Anything',
+                      'mods_directory': 'Anything',
+                      'about': 'Anything',
+                      'manifest': 'Anything',
+                      'steam_id': 'Anything',
+                      'version': 'Anything',
+                      'sort_reversed': ['False','True'],
+                      'sort_tag': ['name','author']
+                      }
+txt_list = lang_pack['text'][app_settings['1']['language']]
+menu_txt = lang_pack['menu'][app_settings['1']['language']]
 deepness_levels = {1:['supportedVersions','loadAfter','targetVersions','loadBefore'],
                    15:['descriptionsByVersion'],
                    2:['modDependencies','dependencies','incompatibleWith'],
                    25:['loadAfterByVersion'],
                    3:['modDependenciesByVersion']}
+main_lists = {'Steam':None,
+              'Manual':None,
+              'Steam_dlc':None,
+              'Manual_dlc':None,
+              'game_versions':[]}
 
-def my_timer(f):  # гикбренс лол)))
-    def tmp(*args, **kwargs):
-        start_time = time.time()
-        result = f(*args, **kwargs)
-        delta_time = time.time() - start_time
-        print('Время выполнения функции {} {}'.format(f,delta_time))
-        return result
-    return tmp
+def about_program():
+    print('-'*80)
+    print(txt_list['about'])
+    print('-'*80+'\n')
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+def menu(menu_lines):
+    for line in menu_lines:
+        print('{}. {}'.format(line,menu_lines[line]))
+def wip():
+    print(txt_list['wip'])
+def wrong_input():
+    print(txt_list['errinp'])
+def settings(inp):
+    if inp not in ['S', 's', 'ы', 'Ы']: return False
+    clear()
+    while True:
+        print(txt_list['settings'])
+        for i in app_settings:
+            for line in app_settings[i]:
+                print('{}. {}: {}'.format(i, line, app_settings[i][line]))
+        print('\n')
+        menu(menu_txt['back_to_main'])
+        i = input(txt_list['param_name'])
+        if i in ['m','M','ь','Ь']:
+            clear()
+            menu(menu_txt['main'])
+            break
+        if app_settings.get(i)!=None:
+            param = []
+            for j in app_settings[i]: param.append(j)
+            print('{}: {}'.format(txt_list['available_param'],available_settings[param[0]]))
+            while True:
+                b = input(txt_list['param_value'])
+                if b in available_settings[j] or available_settings[j]=='Anything':
+                    app_settings[i].update({j:b})
+                    break
+                if b in ['m','M','ь','Ь']: break
+                else:
+                    print(txt_list['err_param'])
+                    continue
 
+        clear()
+        continue
+
+    #else: return False
+def quit_program(inp):
+    if inp in ['Q','q','й','Й']:
+        print(txt_list['q'])
+        quit()
+    else:return False
+def app_main():
+    while True:
+        i = input(txt_list['inp'])
+        if quit_program(i)!=False:continue
+        elif settings(i)!=False:continue
+
+        elif menu_1(i)!=False:continue
+        elif menu_2(i)!=False:continue
+        elif menu_3(i)!=False:continue
+        elif menu_4(i)!=False:continue
+        elif menu_5(i)!=False:continue
+        elif menu_6(i)!=False:continue
+        elif menu_update(i)!=False:continue
+
+        else:
+            wrong_input()
+            continue
 def xmlp(adress):
     def go_deeper(root, tag, deepness_level):
         parsed = {}
@@ -127,16 +213,15 @@ def xmlp(adress):
                 with open(adress) as txt:
                     for line in txt:
                         str_line = string_reaper(line)
-                        parsed.update({re.findall(r'(\w+)\.', adress)[0]: str_line})
+                        parsed.update({re.findall(r'(\w+)\.', adress)[0]: int(str_line)})
                 return parsed
             except Exception as err:
                 return {'Error_txt': [err, adress]}
         else: return {'Error_inp': 'Unknown input'}
     except:return False
-
 def about(mod_directory):
     total_info = {}
-    a,b,c = xmlp(mod_directory + df['about']),xmlp(mod_directory + df['manifest']),xmlp(mod_directory + df['steam_id'])
+    a,b,c = xmlp(mod_directory + app_settings['6']['about']),xmlp(mod_directory + app_settings['7']['manifest']),xmlp(mod_directory + app_settings['8']['steam_id'])
     if a:
         total_info.update(a)
         total_info.update({'directory': mod_directory})
@@ -144,22 +229,19 @@ def about(mod_directory):
     if c: total_info.update(c)
     if a: return total_info
     else: return False
-
 def scanner(mods_directory):
     scanned_list = []
     for mods in os.listdir(mods_directory):
         a = about('{}\{}'.format(mods_directory, mods))
         if a: scanned_list.append(a)
     return scanned_list
-
 def version(game_directory):
     try:
-        with open('{}\{}'.format(game_directory,df['version'])) as ver:
+        with open('{}\{}'.format(game_directory,app_settings['9']['version'])) as ver:
             for id,line in enumerate(ver):
                 if id == 0: return line
                 else: return '0'
     except Exception as err: print('Error_version: {}'.format(err))
-
 def print_list(first_list,second_list=0):
     if second_list==0:
         print("%-4s%-70s%-10s%-50s%-25s%-10s" % ('№','Название мода', 'Версия', 'Автор', 'Поддержка версий', 'Steam ID'))
@@ -186,93 +268,61 @@ def print_list(first_list,second_list=0):
             a,b = f_list.setdefault(i,{}),s_list.setdefault(i,{})
             c = {'name_one':a.setdefault('name',''),'PublishedFileId_one':a.setdefault('PublishedFileId',''),'name_two':b.setdefault('name',''),'PublishedFileId_two':b.setdefault('PublishedFileId','')}
             print("%-4s%-70s%-10s%2s%-4s%-70s%-10s" % (i+1, c['name_one'], c['PublishedFileId_one'], '|', i+1, c['name_two'], c['PublishedFileId_two']))
+def sort_list(list):
+    try:
+        a = sorted(list, key=lambda x: x[app_settings['11']['sort_tag']],reverse=(strtobool(app_settings['10']['sort_reversed'])))
+        return a
+    except Exception as err:
+        print(err)
+        return list
+def update():
+    start_time = time.time()
+    main_lists['Steam'] = scanner('{}'.format(app_settings['2']['steam_mods']))
+    main_lists['Manual'] = scanner('{}{}'.format(app_settings['4']['target_directory'], app_settings['5']['mods_directory']))
+    main_lists['game_versions'].append(version(app_settings['3']['steam_game_dir']))
+    main_lists['game_versions'].append(version(app_settings['4']['target_directory']))
+    delta_time = time.time() - start_time
+    print('Списки обновлены за {} сек.'.format(round(delta_time, 3)))
+
+def menu_1(input):
+    if input == '1': wip()
+    else: return False
+def menu_2(input):
+    if input == '2': wip()
+    else: return False
+def menu_3(input):
+    if input == '3':
+        print_list(sort_list(main_lists['Steam']))
+        print('\n')
+        menu(menu_txt['main'])
+    else: return False
+def menu_4(input):
+    if input == '4':
+        print_list(sort_list(main_lists['Manual']))
+        print('\n')
+        menu(menu_txt['main'])
+    else: return False
+def menu_5(input):
+    if input == '5':
+        tot = main_lists['Steam']+main_lists['Manual']
+
+        print_list(sort_list(tot))
+    else: return False
+def menu_6(input):
+    if input == '6':
+        print_list(sort_list(main_lists['Steam']),sort_list(main_lists['Manual']))
+        print('\n')
+        menu(menu_txt['main'])
+    else: return False
+def menu_update(input):
+    if input in ['U','u','г','Г']:
+        update()
+    else: return False
 
 
-
-
-#test = about('C:\Games\RimWorld\Mods\Test')
-#print(a['supportedVersions'])
-#v = version(df['steam_game_dir'])
-a = scanner('C:/Games/SteamLibrary/steamapps/workshop/content/294100')
-b = scanner('{}{}'.format(df['target_directory'],df['mods_directory']))
-print_list(a,b)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-delta_time = time.time() - start_time
-print('----------------------------------------------')
-print('Время выполнения кода {} секунд.'.format(round(delta_time,3)))
+about_program()
+update()
+menu(menu_txt['main'])
+app_main()
 
 # -------------------------------------------------------------------------------
-'''                for i in param:
-                    a = i.text
-                    b = a.split()
-                    b = ''.join(b)
-                    ver.append(b)
-                return ver'''
-
-'''
-def inputmanager():
-    while True:
-        i = input('Ввод: ')
-        if i == 'Q' or i == 'q' or i == 'й' or i == 'Й':
-            print(txt_list['q'])
-            quit()
-        elif i == '1': print(txt_list['wip'])
-        elif i == '2': print(txt_list['wip'])
-        elif i == '3':
-            j = modlist(0)
-            print('_' * 170)
-            print('{} Модов в Steam - [{}] {}'.format('-'*4,j[0],'-'*140))
-            printlist(j)
-            mainmenuprint(menu)
-        elif i == '4':
-            j = modlist(1)
-            print('_' * 170)
-            print('{} Модов установленных вручную - [{}] {}'.format('-'*4,j[0],'-'*134))
-            printlist(j)
-            mainmenuprint(menu)
-        elif i == '5':
-            j = modlist(2)
-            print('{} Всего модификаций - [{}] {}'.format('-'*4,j[0],'-'*136))
-            printlist(j)
-            mainmenuprint(menu)
-        elif i == '6':
-            print('_' * 170)
-            print("%-4s%-70s%-10s%2s%-4s%-70s%-10s" % ('', 'Установлено в папке Steam', '', '|', '', 'Установлено в папке Rimworld', ''))
-            mods_names_and_folders_steam = dict(scanner(steam_mods, 6))
-            mods_names_and_folders_manual = dict(scanner(rw_manual, 6))
-            mods_in_steam_folder = scanner(steam_mods, 5)
-            mods_in_rimworld_folder = scanner(rw_manual, 5)
-            print(mods_names_and_folders_steam)
-            print(mods_names_and_folders_manual)
-            print(mods_in_steam_folder)
-            print(mods_in_rimworld_folder)
-            printlist(0,1)
-            mainmenuprint(menu)
-        elif i == '7':
-            update()
-            print(txt_list['wip'])
-        elif i == '8': print(txt_list['wip'])
-        else: print(txt_list['errinp'])
-        continue
-def update():
-    #[mods_in_steam_folder, mods_names_and_folders_steam, mods_in_rimworld_folder, mods_names_and_folders_manual]
-    return [scanner(steam_mods, 5), dict(scanner(steam_mods, 6)), scanner(rw_manual, 5), dict(scanner(rw_manual, 6))]
-
-'''
